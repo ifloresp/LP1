ab89e83e67e30b4a7256079733b8f8bd5c1347d6
Iben <ifloresp@ulasalle.edu.pe>
2018-09-25 01:50:08 -0500

    lab2

---
+++ lab 2/cod12.cpp
@@ -0,0 +1,8 @@
+int square(int);
+double sqrt(double);
+int main(){
+	int x=square(44);
+}
+int square(int x){
+	return x*x;
+}
---
+++ lab 2/try_this03.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int value=97;
+	char i;
+	while (value<123){
+	cout<<value<<'\t'<<i<<"\n";
+	i=value;
+	value++;
+}
+}
